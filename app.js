360ad2945cc5d064b816fa6b877fda1c322d6009
YuiChen0207 <asd0935252359@gmail.com>
2023-04-12 20:13:19 +0800

    Optimize code on the router

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const express =  require("express")
-const exphbs = require("express-handlebars")
-const app = express()
-const port = 3000
+const express = require("express");
+const exphbs = require("express-handlebars");
+const { render } = require("express/lib/response");
+const app = express();
+const port = 3000;
 
-app.engine("handlebars", exphbs({defaultLayout: "main"}))
-app.set("view engine", "handlebars")
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index")
-})
+app.get("/:page?", (req, res) => {
+  const pages = ["about", "portfolio", "contact"];
 
-app.get("/about", (req, res) => {
+  if (pages.includes(req.params.page)) {
+    res.render(req.params.page);
+  } else {
+    res.render("index");
+  }
+});
+
+/* app.get("/about", (req, res) => {
   res.render("about");
 });
 
@@ -22,8 +29,8 @@ app.get("/portfolio", (req, res) => {
 
 app.get("/contact", (req, res) => {
   res.render("contact");
-});
+}); */
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`);
-})
+});
